b03c8aad8374a237383bd645bfaa1d10e89bfc4e
Srinayaka S.P.B.M IT24103435 <it24103435@my.sliit.lk>
2026-08-03 11:10:47 +0530

    Lab 01

--- visual_grid_game.py
+++ visual_grid_game.py
@@ -52,18 +52,21 @@ class VisualGridHuntGame:
         self.steps = 0
         self.collision = False
 
-    def get_percept(self) -> dict:
-        return {
-            'agent_pos': list(self.agent_pos),
-            'opponent_positions': [list(op) for op in self.opponents],
-            'smells_food': tuple(self.agent_pos) in self.food_positions,
-            'smells_toxin': tuple(self.agent_pos) in self.toxic_traps,         //implementation2.2
-            'hit_wall': tuple(self.agent_pos) in self.walls,
-            'collision': self.collision,
-            'score': self.score,
-            'remaining_food': len(self.food_positions)
-        }
 
+def get_percept(self) -> dict:
+    return {
+        'agent_pos': list(self.agent_pos),
+        'opponent_positions': [list(op) for op in self.opponents],
+        'smells_food': tuple(self.agent_pos) in self.food_positions,
+        'smells_toxin': tuple(self.agent_pos) in self.toxic_traps,
+        'hit_wall': tuple(self.agent_pos) in self.walls,
+        'collision': self.collision,
+        'score': self.score,
+        'remaining_food': len(self.food_positions)
+    }
+
+
+    
     def execute_action(self, action: str):
         self.steps += 1
         new_pos = list(self.agent_pos)
